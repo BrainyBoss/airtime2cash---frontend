d91696a1f3ee5281456dbc1a54b4e476ee369b4b
mac <mac@BrainyBoss-MacBook-Air.local>
2022-10-13 14:33:38 +0100

    update

--- src/Pages/DashBoard/ThirdTab/Successful/Successful.jsx
+++ src/Pages/DashBoard/ThirdTab/Successful/Successful.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './success.css'
-import {ReactComponent as Mark} from './check.svg'
+// import {ReactComponent as Mark} from './check.svg'
 
 function Successful({closeModal}) {
 
   return (
     <div className='manage_sucess_container'>
         <div className='manage_success'>
-            <Mark className='manage_img' />
+            {/* <Mark className='manage_img' /> */}
         {/* <img className='manage_img' src={Mark} alt="" /> */}
         <h2 className='manage_h2'>Bank Account Successful</h2>
         <p>Your bank account have been added Successfully</p>
